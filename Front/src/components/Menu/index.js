8738208cfa9db53af843f0900f75fe70fd906b0c
Valentin-Dr <valentinderennes@yahoo.fr>
2022-01-18 12:40:00 +0100

    responsive menu

--- Front/src/components/Menu/index.js
+++ Front/src/components/Menu/index.js
@@ -8,42 +8,49 @@ export default function Menu() {
         key="/"
         to="/"
       >
+        <i className="fas fa-home navHidden"></i>
         <button>Accueil</button>
       </NavLink>
       <NavLink
         key="/shop"
         to="/shop"
       >
+        <i className="fas fa-coins navHidden"></i>
         <button>Boutique</button>
       </NavLink>
       <NavLink
         key="/inventory"
         to="/inventory"
       >
+        <i className="fas fa-briefcase navHidden"></i>
         <button>Inventaire / Stats</button>
       </NavLink>
       <NavLink
         key="/fighting"
         to="/fighting"
       >
+        <i className="fas fa-fist-raised navHidden"></i>
         <button>Combat</button>
       </NavLink>
       <NavLink
         key="/craft"
         to="/craft"
       >
+        <i className="fas fa-hammer navHidden"></i>
         <button>Craft</button>
       </NavLink>
       <NavLink
         key="/jobs/mining"
         to="/jobs/mining"
       >
+        <i className="fas fa-toilet-paper navHidden"></i>
         <button>Minage</button>
       </NavLink>
       <NavLink
         key="/jobs/fishing"
         to="/jobs/fishing"
       >
+        <i className="fas fa-fish navHidden"></i>
         <button>Pêche</button>
       </NavLink>
     </div>
