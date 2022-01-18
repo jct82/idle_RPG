379c6d9cfeeb74bdfc7a5dbf9132206031d8f1b6
MathiasBenmansour <mathias70250@gmail.com>
2022-01-18 11:59:10 +0100

    add-Header

--- Front/src/components/App/index.js
+++ Front/src/components/App/index.js
@@ -1,5 +1,6 @@
 // == Import
 
+import Header from './Header';
 import './style.scss';
 
 
@@ -7,7 +8,7 @@ import './style.scss';
 // == Composant
 const App = () => (
   <div className="app">
-  
+  <Header title="Idle-Rpg"/>
    
   </div>
 );
