0b8dd39cb235525e904bc6b6d1c9559add7831c4
MathiasBenmansour <mathias70250@gmail.com>
2022-01-18 09:56:14 +0100

    clean-modele

--- Front/src/components/App/index.js
+++ Front/src/components/App/index.js
@@ -1,15 +1,14 @@
 // == Import
-import reactLogo from './react-logo.svg';
-import './styles.css';
 
-import Counter from '../Counter';
+import './style.scss';
+
+
 
 // == Composant
 const App = () => (
   <div className="app">
-    <img src={reactLogo} alt="react logo" />
-    <h1>Composant : App</h1>
-    <Counter />
+  
+   
   </div>
 );
 
