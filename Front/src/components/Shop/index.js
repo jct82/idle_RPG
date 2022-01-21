15b28e013f1811d278f7167f937bafa7c6fd833d
Castellan-Crowe <frederic.lavialle10@gmail.com>
2022-01-21 15:41:27 +0100

    add responsive

--- Front/src/components/Shop/index.js
+++ Front/src/components/Shop/index.js
@@ -4,12 +4,12 @@ import { v4 as uuidv4 } from 'uuid';
 import { modaleOpen, modaleClose } from '../../actions/shop';
 
 export default function Shop() {
+  const dispatch = useDispatch();
   const { stuffs, isOpen } = useSelector((state) => state.shop);
   // console.log(stuffs);
   // on force le tableau à n'avoir qu'un nombre limité d'élément
   stuffs.length = 9;
   // console.log(stuffs.length);
-  const dispatch = useDispatch();
   // gestion de l'ouverture de la modale
   const openModale = () => {
     dispatch(modaleOpen());
