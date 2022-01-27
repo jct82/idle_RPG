599c6abe7d056719088467b3ef311ab8109a8ece
Castellan-Crowe <frederic.lavialle10@gmail.com>
2022-01-27 15:56:02 +0100

    add axios request on shop page and scss

--- Front/src/store/index.js
+++ Front/src/store/index.js
@@ -4,12 +4,13 @@ import { createStore, compose, applyMiddleware } from 'redux';
 // == Import : local
 import rootReducer from 'src/reducers';
 import logMiddleware from '../middleware/logMiddleware';
+import shopMiddleware from '../middleware/shopMiddleware';
 
 // == Enhancers
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const enhancers = composeEnhancers(
-  applyMiddleware(logMiddleware),
+  applyMiddleware(logMiddleware, shopMiddleware),
 );
 
 // == Store
