9fee8153f0ef06fd69c33d503ff5aa6abe7f24fc
MathiasBenmansour <mathias70250@gmail.com>
2022-01-27 10:43:15 +0100

    modify-css

--- Front/src/components/Character/Stats.js
+++ Front/src/components/Character/Stats.js
@@ -1,13 +1,19 @@
 import { useSelector, useDispatch } from "react-redux";
-import { lifePoints, experience, level, joblevel } from "../../actions/stats";
+import {
+  lifePoints,
+  experience,
+  level,
+  joblevel,
+  force,
+} from "../../actions/stats";
 import "./style.scss";
 import imgLife from "/src/assets/LifePoint/LifePoint.png";
-import force from "/src/assets/Force/force.png";
+import imgForce from "/src/assets/Force/force.png";
 
 export default function Stats() {
   const dispatch = useDispatch();
 
-  const { vie, xp, level, metier, argent,dexterite } = useSelector(
+  const { vie, xp, level, metier, argent, dexterite, force } = useSelector(
     (state) => state.stats
   );
 
@@ -18,30 +24,35 @@ export default function Stats() {
   return (
     <div className="container-stat">
       <ul>
-        <li>
+        <li className="stat-life">
           <span>{vie}</span>
           <img src={imgLife} alt="Coeur" />
         </li>
-        <li>
-          <span> Dextérité : {dexterite}</span>
-          
+
+        <li className="stat-dexterite">
+          <span> {dexterite} Dextérité </span>
         </li>
-        <li>
-          {/* <span>Force</span> */}
-          <img src={force} alt="force" />
+
+        <li className="stat-force">
+          <span>{force}</span>
+          <img src={imgForce} alt="force" />
         </li>
-        <li>
+
+        <li className="stat-experience">
           <span>{xp}</span>
         </li>
-        <li>
+
+        <li className="stat-level">
           <span> Niveau : {level}</span>
         </li>
-        <li>
+
+        <li className="stat-job">
           <span>
-            Metier : {metier} {level}
+            {metier} {level}
           </span>
         </li>
-        <li>
+
+        <li className="stat-money">
           <span>Stock d'or : {argent}</span>
         </li>
       </ul>
